67f9a50a348e6f9a84d780daf64acf4e8cf32dbd
Ankit Yadav <70766998+ankityadav4607@users.noreply.github.com>
2023-10-20 22:23:00 +0530

    Add files via upload

---
+++ tools/train.py
@@ -0,0 +1,587 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# Modified by Sen Yang (yangsenius@seu.edu.cn)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import pprint
+import shutil
+
+import numpy as np
+import random
+
+import torch
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+
+import _init_paths
+from config import cfg
+from config import update_config
+from core.loss import JointsMSELoss
+from core.function import train
+from core.function import validate
+from utils.utils import get_optimizer
+from utils.utils import save_checkpoint
+from utils.utils import create_logger
+from utils.utils import get_model_summary
+
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+from collections import OrderedDict
+
+import copy
+from typing import Optional, List
+
+import torchvision
+BN_MOMENTUM = 0.1
+logger = logging.getLogger(__name__)
+
+import timm
+import math
+import os
+import logging
+import copy
+from typing import Tuple
+from einops import rearrange
+from einops.layers.torch import Rearrange
+from torch.nn import functional as F
+
+davit=timm.create_model('davit_small',pretrained=True)
+
+class PatchExpand(nn.Module):
+    def __init__(self, input_resolution, dim, dim_scale=2, norm_layer=nn.LayerNorm):
+        super().__init__()
+        self.input_resolution = input_resolution
+        self.dim = dim
+        self.expand1 = nn.Linear(dim, 3*dim, bias=False) if dim_scale==2 else nn.Identity()
+        self.expand2 = nn.Linear(3*dim, 2*dim, bias=False) if dim_scale==2 else nn.Identity()
+        self.norm = norm_layer(dim // dim_scale)
+
+    def forward(self, x):
+        """
+        x: B, H*W, C
+        """
+        # print("x_shape-----",x.shape)
+        H, W = self.input_resolution
+        x = self.expand1(x)
+        x=self.expand2(x)
+        
+        B, L, C = x.shape
+        # print(x.shape)
+        assert L == H * W, "input feature has wrong size"
+
+        x = x.view(B, H, W, C)
+        x = rearrange(x, 'b h w (p1 p2 c)-> b (h p1) (w p2) c', p1=2, p2=2, c=C//4)
+        x = x.view(B,-1,C//4)
+        x= self.norm(x.clone())
+
+        return x
+
+class PatchPartition(nn.Module):
+
+    '''
+    input shape -> (b,c,h,w)
+    output shape -> (b, (h * w), C)
+    '''
+
+    def __init__(self, patch_size=1, C=dim):
+        super().__init__()
+        self.linear_embedding = nn.Conv2d(3, C, kernel_size=patch_size, stride=patch_size)
+        self.layer_norm = nn.LayerNorm(C)
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        x = self.linear_embedding(x)
+        x = rearrange(x, 'b c h w -> b (h w) c')
+        x = self.relu(self.layer_norm(x))
+        return x
+       
+class Block_1(nn.Module):
+    """ 
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+    """
+    def __init__(self, dim, drop_path=0.):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=3, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+class Block_2(nn.Module):
+    """ 
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+    """
+    def __init__(self, dim, drop_path=0.):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=5, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+        
+class Block_3(nn.Module):
+    """ 
+    Args:
+        dim (int): Number of input channels.
+        drop_path (float): Stochastic depth rate. Default: 0.0
+    """
+    def __init__(self, dim, drop_path=0.):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
+        self.norm = LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.grn = GRN(4 * dim)
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+
+        x = input + self.drop_path(x)
+        return x
+
+
+class Attention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*x
+        out=spatial_out+channel_out
+        return out
+
+
+
+
+
+
+class SequentialAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(channel_out) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(channel_out) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*channel_out
+        return spatial_out
+
+class HumanPoseNet(nn.Module):
+    def __init__(self,davit):
+        super().__init__()
+        self.l0=davit.stem
+        self.backbone=davit.stages[0:4]
+        
+        self.patchExpand_1=PatchExpand((8,6),768)
+        self.patchExpand_2=PatchExpand((16,12),384)
+        self.patchExpand_3=PatchExpand((32,24),192)
+        
+        
+        self.patchPartition_1 = PatchPartition(1,768)
+        self.patchPartition_2 = PatchPartition(1,384)
+        self.patchPartition_3 = PatchPartition(1,192)
+        
+        self.norm_1 = nn.BatchNorm2d(384)
+        self.norm_2 = nn.BatchNorm2d(192)
+        self.norm_3 = nn.BatchNorm2d(96)
+        
+        
+        self.act_1 = nn.ReLU()
+        self.act_2 = nn.ReLU()
+        self.act_3 = nn.ReLU()
+        
+        
+        self.conv_1x1=nn.Sequential(nn.ConvTranspose2d(96,17,1,1))
+        
+        
+        self.attention_1=Attention(channel=384)
+        self.attention_2=Attention(channel=192)
+        self.attention_3=Attention(channel=97)
+        self.attentionalHead=Attention(channel=17)
+        
+        
+        self.refinement_1 = Block_1(dim=384)
+        self.refinement_2 = Block_2(dim=192)
+        self.refinement_3 = Block_3(dim=96)
+        
+        
+    def forward(self,x):
+    #Encoder
+        x=self.l0(x)
+        x=self.backbone(x)
+        
+    #Decoder
+        B,C,H,W=x.shape
+        x=self.patchPartition_1(x)
+        x=self.patchExpand_1(x)
+        x=torch.reshape(x,(B,384,16,12))
+        x=self.norm_1(x)
+        x=self.act_1(x)
+        x=self.refinement_1(x)
+        x=self.attention_1(x)
+        
+        x=self.patchPartition_2(x)
+        x=self.patchExpand_2(x)
+        x=torch.reshape(x,(B,192,32,24))
+        x=self.norm_2(x)
+        x=self.act_2(x)
+        x=self.refinement_2(x)
+        x=self.attention_2(x)
+        
+        x=self.patchPartition_3(x)
+        x=self.patchExpand_3(x)   
+        x=torch.reshape(x,(B,96,64,48))
+        x=self.norm_3(x)
+        x=self.act_3(x)
+        x=self.refinement_3(x)
+        x=self.attention_3(x)
+        
+        x=self.conv_1x1(x)
+        x=self.attentionalHead(x)
+        
+        return x
+
+def get_pose_net():
+   
+    model = HumanPoseNet(davit)
+   
+    return model
+
+       
+import dataset
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train keypoints network')
+    # general
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+
+    # philly
+    parser.add_argument('--modelDir',
+                        help='model directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--logDir',
+                        help='log directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--dataDir',
+                        help='data directory',
+                        type=str,
+                        default='')
+    parser.add_argument('--prevModelDir',
+                        help='prev Model directory',
+                        type=str,
+                        default='')
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    update_config(cfg, args)
+
+    logger, final_output_dir, tb_log_dir = create_logger(
+        cfg, args.cfg, 'train')
+
+    logger.info(pprint.pformat(args))
+    logger.info(cfg)
+
+    # cudnn related setting
+    cudnn.benchmark = cfg.CUDNN.BENCHMARK
+    torch.backends.cudnn.deterministic = cfg.CUDNN.DETERMINISTIC
+    torch.backends.cudnn.enabled = cfg.CUDNN.ENABLED
+
+    seed = 22
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    model = get_pose_net()
+
+    # copy model file
+    this_dir = os.path.dirname(__file__)
+    shutil.copy2(
+        os.path.join(this_dir, '../lib/models', cfg.MODEL.NAME + '.py'),
+        final_output_dir)
+    # logger.info(pprint.pformat(model))
+
+    writer_dict = {
+        'writer': SummaryWriter(log_dir=tb_log_dir),
+        'train_global_steps': 0,
+        'valid_global_steps': 0,
+    }
+
+    dump_input = torch.rand(
+        (1, 3, cfg.MODEL.IMAGE_SIZE[1], cfg.MODEL.IMAGE_SIZE[0])
+    )
+
+    model = torch.nn.DataParallel(model, device_ids=cfg.GPUS).cuda()
+    print(model)
+    
+
+    # define loss function (criterion) and optimizer
+    criterion = JointsMSELoss(
+        use_target_weight=cfg.LOSS.USE_TARGET_WEIGHT
+    ).cuda()
+
+    # Data loading code
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+    train_dataset = eval('dataset.'+cfg.DATASET.DATASET)(
+        cfg, cfg.DATASET.ROOT, cfg.DATASET.TRAIN_SET, True,
+        transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+    valid_dataset = eval('dataset.'+cfg.DATASET.DATASET)(
+        cfg, cfg.DATASET.ROOT, cfg.DATASET.TEST_SET, False,
+        transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+    )
+    
+    batch_size=cfg.TRAIN.BATCH_SIZE_PER_GPU*len(cfg.GPUS)
+
+
+    best_perf = 0.0
+    best_model = False
+    last_epoch = -1
+
+    optimizer = get_optimizer(cfg, model)
+
+    begin_epoch = cfg.TRAIN.BEGIN_EPOCH
+    checkpoint_file = os.path.join(
+        final_output_dir, 'checkpoint.pth'
+    )
+
+    if cfg.AUTO_RESUME and os.path.exists(checkpoint_file):
+        logger.info("=> loading checkpoint '{}'".format(checkpoint_file))
+        checkpoint = torch.load(checkpoint_file)
+        begin_epoch = checkpoint['epoch']
+        best_perf = checkpoint['perf']
+        last_epoch = checkpoint['epoch']
+
+        writer_dict['train_global_steps'] = checkpoint['train_global_steps']
+        writer_dict['valid_global_steps'] = checkpoint['valid_global_steps']
+
+        model.load_state_dict(checkpoint['state_dict'])
+
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        logger.info("=> loaded checkpoint '{}' (epoch {})".format(
+            checkpoint_file, checkpoint['epoch']))
+
+    # lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+    #     optimizer, cfg.TRAIN.LR_STEP, cfg.TRAIN.LR_FACTOR,
+    #     last_epoch=last_epoch
+    # )
+
+    
+    
+    #lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+     #   optimizer, cfg.TRAIN.END_EPOCH, eta_min=cfg.TRAIN.LR_END, last_epoch=last_epoch)
+    number_warmup_epochs=5
+    train_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, cfg.TRAIN.END_EPOCH, eta_min=cfg.TRAIN.LR_END, last_epoch=last_epoch)
+        
+    def warmup(current_step: int):
+        return 0.0001* (float(number_warmup_epochs - current_step))
+        
+    warmup_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=warmup)
+
+    lr_scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, [warmup_scheduler, train_scheduler], [number_warmup_epochs])
+
+    model.cuda()
+
+
+    for epoch in range(begin_epoch, cfg.TRAIN.END_EPOCH):
+        
+        if (epoch-2)%3==0 and epoch>=5:
+            batch_size+=2
+        train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size*len(cfg.GPUS),
+        shuffle=cfg.TRAIN.SHUFFLE,
+        num_workers=cfg.WORKERS,
+        pin_memory=cfg.PIN_MEMORY
+    )
+        valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=cfg.TEST.BATCH_SIZE_PER_GPU*len(cfg.GPUS),
+        shuffle=False,
+        num_workers=cfg.WORKERS,
+        pin_memory=cfg.PIN_MEMORY
+    )
+
+        logger.info("=> current learning rate is {:.6f}".format(lr_scheduler.get_last_lr()[0]))
+        # train for one epoch
+        train(cfg, train_loader, model, criterion, optimizer, epoch,
+              final_output_dir, tb_log_dir, writer_dict)
+
+        # evaluate on validation set
+        perf_indicator = validate(
+            cfg, valid_loader, valid_dataset, model, criterion,
+            final_output_dir, tb_log_dir, writer_dict
+        )
+        
+        lr_scheduler.step()
+
+        if perf_indicator >= best_perf:
+            best_perf = perf_indicator
+            best_model = True
+        else:
+            best_model = False
+
+        logger.info('=> saving checkpoint to {}'.format(final_output_dir))
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'model': cfg.MODEL.NAME,
+            'state_dict': model.state_dict(),
+            'best_state_dict': model.module.state_dict(),
+            'perf': perf_indicator,
+            'optimizer': optimizer.state_dict(),
+            'train_global_steps': writer_dict['train_global_steps'],
+            'valid_global_steps': writer_dict['valid_global_steps'],
+        }, best_model, final_output_dir)
+
+    final_model_state_file = os.path.join(
+        final_output_dir, 'final_state.pth'
+    )
+    logger.info('=> saving final model state to {}'.format(
+        final_model_state_file)
+    )
+    torch.save(model.module.state_dict(), final_model_state_file)
+    writer_dict['writer'].close()
+
+
+if __name__ == '__main__':
+    main()
